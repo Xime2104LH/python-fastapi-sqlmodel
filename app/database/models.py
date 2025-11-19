f794dad6a10f03f4442b478aa27da88f815f3950
XimenaLH21 <ximena@dicalzeus.com>
2025-11-19 09:49:06 -0700

    Add new model Category and response model TaskWithCategory

--- app/database/models.py
+++ app/database/models.py
@@ -6,8 +6,18 @@ class Task(SQLModel, table=True):
     title: str
     description: str
     completed: bool | None = Field(default=False)
+    category_id: int | None = Field(default=None, foreign_key="category.id")
 
 class TaskUpdate(SQLModel):
     title: str | None = None
     description: str | None = None
     completed: bool | None = None
+
+class Category(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+
+class TaskWithCategory(SQLModel):
+    task: Task
+    category: Category | None = None
+
